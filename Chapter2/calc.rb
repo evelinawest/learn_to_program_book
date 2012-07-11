16c583218d447767e8bbb3cd04ec65e770c88b67
Evelina West <blastoff@onewestway.com>
2012-07-11 19:28:40 -0400

    Better code organization.

---
+++ Chapter2/calc.rb
@@ -0,0 +1,12 @@
+puts 1+2
+puts 1.0 + 2.0
+puts 1+3
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+puts 1+2
+puts 2*3
+puts 9/2
+puts 5 * (12-8) + -15
+puts 98 + (59872) / (13*8) * -51
+puts 9.4 + 4.0
---
+++ Chapter5/chomp.rb
@@ -0,0 +1,4 @@
+puts ' Hello there, and what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + evelina + '? What a lovely name!'
+puts 'Pleased to meet you, ' + evelina +' . :-)o
