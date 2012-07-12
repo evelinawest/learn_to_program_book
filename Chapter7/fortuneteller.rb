ad9623989d485ff78e409f11fd73d3d5d982a852
Evelina West <blastoff@onewestway.com>
2012-07-12 10:32:00 -0400

    I just added the fortuneteller program to my Chapter7 directory.

---
+++ Chapter7/fortuneteller.rb
@@ -0,0 +1,7 @@
+puts 'I am a fortune teller. Tell me your name:'
+name = gets.chomp
+if name == 'Evelina'
+  puts 'I see great things in your future.'
+  puts 'Your future is...oh! Look at the time!'
+  puts 'I really have to go ,sorry!'
+end   
