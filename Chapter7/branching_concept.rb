17dcb4c5edf0e80fcf6193a362ccc2216a6ce891
Evelina West <blastoff@onewestway.com>
2012-07-20 20:07:31 -0400

    Updates to Chapter7.

--- Chapter7/branching_concept.rb
+++ Chapter7/branching_concept.rb
@@ -1,6 +1,16 @@
-puts 'Hello, what\'s your name?'
-name = gets.chomp
-puts 'Hello, ' + name +'.'
-if name == 'Chewbacca'
-  puts 'What a lovely name!'
+
+
+puts 'Hello and welcome to seventh grade english.'
+puts 'My name is Miss Gabard.  And your name is....?'
+ name = gets.chomp
+if name == name.capitalize
+  puts 'Please take a seat, ' + name + '.'
+else 
+  puts name + '?  You mean ' + name.capitalize + ', right?'
+  puts 'Don\'t you even know how to speel your own name??' 
+  reply = gets.chomp
+  if reply.downcase == 'yes'
+  else 
+  end 
 end
+  
--- Chapter7/fortuneteller.rb
+++
@@ -1,7 +0,0 @@
-puts 'I am a fortune teller. Tell me your name:'
-name = gets.chomp
-if name == 'Evelina'
-  puts 'I see great things in your future.'
-  puts 'Your future is...oh! Look at the time!'
-  puts 'I really have to go ,sorry!'
-end   
