66846747026d16f79a19accbcb6883119b9034b3
Evelina West <blastoff@onewestway.com>
2012-09-26 15:24:40 -0400

    Adding looping program

---
+++ Chapter7/looping.rb
@@ -0,0 +1,21 @@
+input = ''
+while input != 'bye'
+  puts input 
+  input = gets.chomp
+end 
+puts 'Come again soon!'
+
+
+
+# THIS IS TOTALLY A REAL PROGRAM!
+while 'Spike' > 'Angel' 
+  input = gets.chomp
+  puts input
+  if input == 'bye'
+    break 
+  end 
+end 
+puts 'Come again soon!'
+ 
+
+
